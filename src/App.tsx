a9d3d6bd81c99e1bfdd05a0f901c328f9e256e69
tarunpradeep <tarunpradeep2005@gmail.com>
2026-03-10 00:02:37 +0530

    AdminLogin path added to routing in Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./MainUI/Dashboard";
 import Navigation from "./MainUI/Navigation";
 import ViewMap from "./MainUI/ViewMap";
+import AdminLogin from "./MainUI/AdminLogin";
 import "./index.css";
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Route path="/" element={<Dashboard />} />
       <Route path="/navigate" element={<Navigation />} />
       <Route path="/map" element={<ViewMap />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
     </Routes>
   );
 };
